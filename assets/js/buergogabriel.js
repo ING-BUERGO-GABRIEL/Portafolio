aa1e8d86b587522bba377d5cfe5f88ef54801203
ING-BUERGO-GABRIEL <gabrielbuergo@gmail.com>
2024-01-20 11:42:01 -0400

    Mis Servicios

--- assets/js/buergogabriel.js
+++ assets/js/buergogabriel.js
@@ -1,160 +1,68 @@
 /*!
 =========================================================
-* BUERGO GABRIEL page
+* BUERGO GABRIEL Page
 =========================================================
 */
 
 // smooth scroll
-$(document).ready(function(){
-    $(".navbar .nav-link").on('click', function(event) {
-
-        if (this.hash !== "") {
-
-            event.preventDefault();
-
-            var hash = this.hash;
-
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 700, function(){
-                window.location.hash = hash;
-            });
-        } 
-    });
+$(document).ready(function () {
+  $(".navbar .nav-link").on('click', function (event) {
+      if (this.hash !== "") {
+          event.preventDefault();
+          var hash = this.hash;
+          $('html, body').animate({
+              scrollTop: $(hash).offset().top
+          }, 700, function () {
+              window.location.hash = hash;
+          });
+      }
+  });
 });
 
 // protfolio filters
-$(window).on("load", function() {
-    var t = $(".portfolio-container");
-    t.isotope({
-        filter: ".new",
-        animationOptions: {
-            duration: 750,
-            easing: "linear",
-            queue: !1
-        }
-    }), $(".filters a").click(function() {
-        $(".filters .active").removeClass("active"), $(this).addClass("active");
-        var i = $(this).attr("data-filter");
-        return t.isotope({
-            filter: i,
-            animationOptions: {
-                duration: 750,
-                easing: "linear",
-                queue: !1
-            }
-        }), !1
-    });
+$(window).on("load", function () {
+  var t = $(".portfolio-container");
+  t.isotope({
+      filter: ".new",
+      animationOptions: {
+          duration: 750,
+          easing: "linear",
+          queue: !1
+      }
+  }), $(".filters a").click(function () {
+      $(".filters .active").removeClass("active"), $(this).addClass("active");
+      var i = $(this).attr("data-filter");
+      return t.isotope({
+          filter: i,
+          animationOptions: {
+              duration: 750,
+              easing: "linear",
+              queue: !1
+          }
+      }), !1
+  });
+
+  // Calcular la edad y mostrarla
+  calcularYMostrarEdad();
 });
 
+// Función para calcular y mostrar la edad
+function calcularYMostrarEdad() {
+  var fechaNacimiento = new Date("09/25/1996");
+  var fechaActual = new Date();
 
-// google maps
-function initMap() {
-// Styles a map in night mode.
-    var map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 40.674, lng: -73.945},
-        zoom: 12,
-        scrollwheel:  false,
-        navigationControl: false,
-        mapTypeControl: false,
-        scaleControl: false,
-      styles: [
-        {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
-        {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
-        {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
-        {
-          featureType: 'administrative.locality',
-          elementType: 'labels.text.fill',
-          stylers: [{color: '#d59563'}]
-        },
-        {
-          featureType: 'poi',
-          elementType: 'labels.text.fill',
-          stylers: [{color: '#d59563'}]
-        },
-        {
-          featureType: 'poi.park',
-          elementType: 'geometry',
-          stylers: [{color: '#263c3f'}]
-        },
-        {
-          featureType: 'poi.park',
-          elementType: 'labels.text.fill',
-          stylers: [{color: '#6b9a76'}]
-        },
-        {
-          featureType: 'road',
-          elementType: 'geometry',
-          stylers: [{color: '#38414e'}]
-        },
-        {
-          featureType: 'road',
-          elementType: 'geometry.stroke',
-          stylers: [{color: '#212a37'}]
-        },
-        {
-          featureType: 'road',
-          elementType: 'labels.text.fill',
-          stylers: [{color: '#9ca5b3'}]
-        },
-        {
-          featureType: 'road.highway',
-          elementType: 'geometry',
-          stylers: [{color: '#746855'}]
-        },
-        {
-          featureType: 'road.highway',
-          elementType: 'geometry.stroke',
-          stylers: [{color: '#1f2835'}]
-        },
-        {
-          featureType: 'road.highway',
-          elementType: 'labels.text.fill',
-          stylers: [{color: '#f3d19c'}]
-        },
-        {
-          featureType: 'transit',
-          elementType: 'geometry',
-          stylers: [{color: '#2f3948'}]
-        },
-        {
-          featureType: 'transit.station',
-          elementType: 'labels.text.fill',
-          stylers: [{color: '#d59563'}]
-        },
-        {
-          featureType: 'water',
-          elementType: 'geometry',
-          stylers: [{color: '#17263c'}]
-        },
-        {
-          featureType: 'water',
-          elementType: 'labels.text.fill',
-          stylers: [{color: '#515c6d'}]
-        },
-        {
-          featureType: 'water',
-          elementType: 'labels.text.stroke',
-          stylers: [{color: '#17263c'}]
-        }
-      ]
-    });
-    
-// Calcular la edad y mostrarla
-var fechaNacimiento = new Date("09/25/1996");
-var fechaActual = new Date();
+  var edad = fechaActual.getFullYear() - fechaNacimiento.getFullYear();
 
-var edad = fechaActual.getFullYear() - fechaNacimiento.getFullYear();
+  // Comprobar si ya ha pasado el cumpleaños de este año
+  if (
+      fechaNacimiento.getMonth() > fechaActual.getMonth() ||
+      (fechaNacimiento.getMonth() === fechaActual.getMonth() &&
+          fechaNacimiento.getDate() > fechaActual.getDate())
+  ) {
+      edad--;
+  }
 
-// Comprobar si ya ha pasado el cumpleaños de este año
-if (
-    fechaNacimiento.getMonth() > fechaActual.getMonth() ||
-    (fechaNacimiento.getMonth() === fechaActual.getMonth() &&
-        fechaNacimiento.getDate() > fechaActual.getDate())
-) {
-    edad--;
+  // Mostrar la edad en el elemento correspondiente
+  document.getElementById("edad").innerText = ` ${edad} años`;
 }
 
-// Mostrar la edad en el elemento correspondiente
-document.getElementById("edad").innerText = `Edad :  sss ${edad} años`;
-}
